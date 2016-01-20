50efaae29f376135eb0856125e3f1fee00bfb448
WeiGrand <913244646@qq.com>
2016-01-20 22:17:50 +0800

    update

--- codewar.js
+++ codewar.js
@@ -105,3 +105,11 @@ function validBraces(braces){
 
   return true;
 }
+
+//a better way
+function validBraces(braces){
+  while(braces.indexOf("{}") != -1 || braces.indexOf("()") != -1 || braces.indexOf("[]") != -1){
+    braces = braces.replace("{}", "").replace("()", "").replace("[]", "");
+  }
+  return braces.length == 0;
+}
